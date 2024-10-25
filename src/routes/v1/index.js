924ec69ebf3fcd3e904a9cf67b6e1bc351ba31c6
sanfarish <farisfalah@gmail.com>
2024-10-25 23:59:27 +0700

    init swagger ui for documentation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const swaggerUI = require('swagger-ui-express');
+const docs = require('../../docs/v1/openapi.json');
 
 const category = require("./category.route");
 const payments = require("./payments.route");
@@ -10,5 +12,6 @@ app.use("/category", category);
 app.use("/payments", payments);
 app.use("/menus", menus);
 app.use("/orders", orders);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
 
 module.exports = app;
