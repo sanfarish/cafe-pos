1dfa9c5f91a3e0af353b4d87b77c9e95cbd73838
sanfarish <farisfalah@gmail.com>
2024-10-29 23:59:03 +0700

    add example in frontend with material ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import Button from '@mui/material/Button'
+import { Container, Paper, Typography } from '@mui/material'
+
+const serviceList = ["Service 1", "Service 2", "Service 3"]
 
 function App() {
-
   return (
-    <>
-      <Button variant="contained">Hello world</Button>
-    </>
+    <Container>
+      <Typography
+        variant='h1'
+        sx={{ my: 4, textAlign: "center", color: "primary.main"}}
+      >
+        Services
+      </Typography>
+      <Typography variant='h2'>Overview</Typography>
+      {serviceList.map((service) => (
+        <Paper elevation={3}>
+          <Typography variant='h3'>{service}</Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, vero? Rem, necessitatibus ab? Illo, esse. Natus sunt velit iusto ad qui saepe quaerat eligendi omnis! Ipsam, error cupiditate. Exercitationem a repudiandae odio recusandae dolore aspernatur. Inventore repellendus vero officiis assumenda reiciendis! Fugiat ullam, beatae magnam, impedit voluptates exercitationem maiores aliquam aut quaerat nobis distinctio a, quas eum modi cumque sequi. Nobis ut quia optio quas voluptate animi, eos sequi aspernatur eligendi natus molestiae minus quos voluptatem incidunt sint asperiores aliquid maxime id veritatis laboriosam voluptas odio repellat commodi! Rem quae minus vel asperiores atque velit officiis repellat impedit deleniti quas.
+          </Typography>
+        </Paper>
+      ))}
+    </Container>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#013e87"
+    },
+    secondary: {
+      main: "#2e74c9"
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: 600,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
