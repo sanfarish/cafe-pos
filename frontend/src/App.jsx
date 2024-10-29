5d257486dc643d5383c02ee5e4570a08774d72f7
sanfarish <farisfalah@gmail.com>
2024-10-30 00:27:17 +0700

    update frontend, add concurrency to github workflows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Container, Paper, Typography } from '@mui/material'
+import { Box, Button, Container, Paper, Typography } from '@mui/material'
 
 const serviceList = ["Service 1", "Service 2", "Service 3"]
 
@@ -12,14 +12,30 @@ function App() {
         Services
       </Typography>
       <Typography variant='h2'>Overview</Typography>
-      {serviceList.map((service) => (
-        <Paper elevation={3}>
-          <Typography variant='h3'>{service}</Typography>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, vero? Rem, necessitatibus ab? Illo, esse. Natus sunt velit iusto ad qui saepe quaerat eligendi omnis! Ipsam, error cupiditate. Exercitationem a repudiandae odio recusandae dolore aspernatur. Inventore repellendus vero officiis assumenda reiciendis! Fugiat ullam, beatae magnam, impedit voluptates exercitationem maiores aliquam aut quaerat nobis distinctio a, quas eum modi cumque sequi. Nobis ut quia optio quas voluptate animi, eos sequi aspernatur eligendi natus molestiae minus quos voluptatem incidunt sint asperiores aliquid maxime id veritatis laboriosam voluptas odio repellat commodi! Rem quae minus vel asperiores atque velit officiis repellat impedit deleniti quas.
-          </Typography>
-        </Paper>
-      ))}
+      <Box
+        sx={{
+          pt: 4,
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: "space-between",
+          gap: 4,
+        }}
+      >
+        {serviceList.map((service) => (
+          // eslint-disable-next-line react/jsx-key
+          <Paper elevation={3} sx={{ width: { xs: 1, md: 320 } }}>
+            <Box sx={{ m: 3 }}>
+              <Typography variant='h3'>{service}</Typography>
+              <Typography sx={{ mt: 2 }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eaque facere voluptatum! In dignissimos maxime nostrum saepe asperiores rem officia commodi, quis consequatur, ut autem inventore perspiciatis illo, sequi ad dolor distinctio velit! Quae sunt corrupti iste molestias? Doloribus illum nobis exercitationem quas amet consectetur ex sed voluptates suscipit inventore!
+              </Typography>
+              <Button variant='contained' color='secondary' sx={{ mt: 2 }}>
+                Learn More
+              </Button>
+            </Box>
+          </Paper>
+        ))}
+      </Box>
     </Container>
   )
 }
