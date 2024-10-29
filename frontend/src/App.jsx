967bdff5b8a3960169425fafc706808060d0c865
sanfarish <farisfalah@gmail.com>
2024-10-30 00:57:09 +0700

    frontend: add key to map function, workflows: minor fix of concurrency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { Box, Button, Container, Paper, Typography } from '@mui/material'
 
-const serviceList = ["Service 1", "Service 2", "Service 3"]
 
 function App() {
+
+  const serviceList = ["Service 1", "Service 2", "Service 3"]
+
   return (
     <Container>
       <Typography
@@ -21,20 +23,21 @@ function App() {
           gap: 4,
         }}
       >
-        {serviceList.map((service) => (
-          // eslint-disable-next-line react/jsx-key
-          <Paper elevation={3} sx={{ width: { xs: 1, md: 320 } }}>
-            <Box sx={{ m: 3 }}>
-              <Typography variant='h3'>{service}</Typography>
-              <Typography sx={{ mt: 2 }}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eaque facere voluptatum! In dignissimos maxime nostrum saepe asperiores rem officia commodi, quis consequatur, ut autem inventore perspiciatis illo, sequi ad dolor distinctio velit! Quae sunt corrupti iste molestias? Doloribus illum nobis exercitationem quas amet consectetur ex sed voluptates suscipit inventore!
-              </Typography>
-              <Button variant='contained' color='secondary' sx={{ mt: 2 }}>
-                Learn More
-              </Button>
-            </Box>
-          </Paper>
-        ))}
+        {serviceList.map((service) => {
+          return (
+            <Paper elevation={3} sx={{ width: { xs: 1, md: 320 } }} key={serviceList.indexOf(service)}>
+              <Box sx={{ m: 3 }}>
+                <Typography variant='h3'>{service}</Typography>
+                <Typography sx={{ mt: 2 }}>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.
+                </Typography>
+                <Button variant='contained' color='secondary' sx={{ mt: 2 }}>
+                  Learn More
+                </Button>
+              </Box>
+            </Paper>
+          )
+        })}
       </Box>
     </Container>
   )
