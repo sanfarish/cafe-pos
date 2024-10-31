871392a378a3760cc901414d7ef6c20d03e9baa9
sanfarish <farisfalah@gmail.com>
2024-10-31 23:39:45 +0700

    update card design, add baselinecss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import { Box, Button, Container, Paper, Typography } from '@mui/material'
 
-
 function App() {
 
-  const serviceList = ["Service 1", "Service 2", "Service 3"]
+  const menus = [
+    {
+      name: "Sandwich",
+      pic: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.",
+    },
+    {
+      name: "Coffee Latte",
+      pic: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.",
+    },
+    {
+      name: "Fruit Salad",
+      pic: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.",
+    },
+    {
+      name: "Pizza",
+      pic: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.",
+    },
+    {
+      name: "Milk Tea",
+      pic: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.",
+    },
+  ]
 
   return (
     <Container>
@@ -16,28 +36,36 @@ function App() {
       <Typography variant='h2'>Overview</Typography>
       <Box
         sx={{
-          pt: 4,
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          justifyContent: "space-between",
-          gap: 4,
+          justifyContent: "center",
         }}
       >
-        {serviceList.map((service) => {
-          return (
-            <Paper elevation={3} sx={{ width: { xs: 1, md: 320 } }} key={serviceList.indexOf(service)}>
-              <Box sx={{ m: 3 }}>
-                <Typography variant='h3'>{service}</Typography>
-                <Typography sx={{ mt: 2 }}>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur laborum quos consequatur iste itaque dolores sapiente repellat praesentium nam reiciendis ipsum voluptas atque natus autem doloribus, quo aspernatur quis labore.
-                </Typography>
-                <Button variant='contained' color='secondary' sx={{ mt: 2 }}>
-                  Learn More
-                </Button>
-              </Box>
-            </Paper>
-          )
-        })}
+        <Box
+          sx={{
+            width: { xs: 320, md: 672, lg: 1024 },
+            pt: 4,
+            pb: 4,
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 4,
+          }}
+        >
+          {menus.map((menu) => {
+            return (
+              <Paper elevation={3} sx={{ width: 320, textAlign: "center" }} key={menus.indexOf(menu)}>
+                <Box sx={{ m: 3 }}>
+                  <Typography variant='h3'>{menu.name}</Typography>
+                  <Typography sx={{ mt: 2 }}>
+                    {menu.pic}
+                  </Typography>
+                  <Button variant='contained' color='secondary' sx={{ mt: 2 }}>
+                    Add
+                  </Button>
+                </Box>
+              </Paper>
+            )
+          })}
+        </Box>
       </Box>
     </Container>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createTheme, ThemeProvider } from '@mui/material'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 
 const theme = createTheme({
   palette: {
@@ -32,6 +32,7 @@ const theme = createTheme({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </StrictMode>,
