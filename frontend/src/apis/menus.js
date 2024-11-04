1fe7270c3c45d5fd4d7ecfd2ef86ee245e953873
sanfarish <farisfalah@gmail.com>
2024-11-04 14:53:31 +0700

    frontend: minor fix menus apis

--- frontend/src/apis/menus.js
+++ frontend/src/apis/menus.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 async function getMenus() {
-    const urlDev = "http://localhost:3001/api/v1"
-    // const urlProd = "https://cafepos.farishasan.web.id/api/v1"
-    const res = await axios.get(urlDev + "/menus")
-    // const res = await axios.get(urlProd + "/menus")
+    // const urlDev = "http://localhost:3001/api/v1"
+    const urlProd = "https://cafepos.farishasan.web.id/api/v1"
+    // const res = await axios.get(urlDev + "/menus")
+    const res = await axios.get(urlProd + "/menus")
     const data = res.data.map(item => {
-        item.image = urlDev + "/images/" + item.image
-        // item.image = urlProd + "/images/" + item.image
+        // item.image = urlDev + "/images/" + item.image
+        item.image = urlProd + "/images/" + item.image
         return item
     })
     return data
