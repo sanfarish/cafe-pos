66ade4b1908051a2cc8d10157a011a88a490fc34
sanfarish <farisfalah@gmail.com>
2024-11-13 20:57:43 +0700

    update CI/CD|backend: rerouting for convenience, update config

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const logger = require("morgan");
 
-const v1 = require("./routes/v1");
+const api = require("./routes");
 
 const notFound = require("./middlewares/notFoundHandler");
 const errors = require("./middlewares/errorsHandler");
@@ -13,8 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/favicon.ico", express.static("public/favicon.ico"));
-app.get([ "/api", "/api/v1" ], (req, res) => res.redirect(302, "/api/v1/docs"));
-app.use("/api/v1", v1);
+app.use("/api", api);
 
 app.use(errors);
 app.use("*", notFound);
--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -14,5 +14,6 @@ router.use("/payments", payments);
 router.use("/menus", menus);
 router.use("/orders", orders);
 router.use("/docs", swaggerUI.serve, swaggerUI.setup(docs));
+router.use("/", (req, res) => res.redirect(301, "/api/v1/docs"));
 
 module.exports = router;
