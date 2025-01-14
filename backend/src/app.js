91e839cf757ca1113a9af934c983731f98c62817
sanfarish <farisfalah@gmail.com>
2025-01-14 22:32:38 +0700

    backend: add security best practices

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const app = express();
+const compression = require('compression');
 const cors = require("cors");
+const helmet = require('helmet');
 const logger = require("morgan");
+const app = express();
 
 const api = require("./routes");
 
 const notFound = require("./middlewares/notFoundHandler");
 const errors = require("./middlewares/errorsHandler");
 
-app.use(logger("dev"));
+app.use(logger("common"));
+app.use(helmet());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(compression());
 app.use(express.json());
-app.use(cors());
 
 app.use("/favicon.ico", express.static("public/favicon.ico"));
 app.use("/api", api);
